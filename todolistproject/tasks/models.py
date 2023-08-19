30deb1e18845daaeceb008579394a5eadd1416b7
Semyon Ivanov <semeni07@mail.ru>
2023-08-19 22:21:47 +0300

    List

--- todolistproject/tasks/models.py
+++ todolistproject/tasks/models.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 class Task(models.Model):
     name = models.CharField(max_length=50, blank=False)
-    description = models.CharField(max_length=400, blank=False)
-    creation_date = models.DateField(default=datetime.now)
+    description = models.CharField(max_length=100, blank=False)
+    is_done = models.BooleanField(default=False, blank=False)
+    creation_date = models.DateField(default=datetime.now, blank=True)
 
     def __str__(self):
         return f"Name: {self.name} / Description: {self.description} / Date: {self.creation_date}"
--- todolistproject/tasks/views.py
+++ todolistproject/tasks/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from tasks.models import Task
+
 
 def main_page(request):
-    return render(request, 'index.html')
+    completed_tasks = Task.objects.filter(is_done=True)
+    tasks_todo = Task.objects.filter(is_done=False)
+
+    return render(request=request,
+                  template_name='index.html',
+                  context={'completed_tasks': completed_tasks,
+                           'tasks_todo': tasks_todo})
