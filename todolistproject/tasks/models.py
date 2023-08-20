5d85064a15b38120a311cf3a011e6f3807ca6cdc
Semyon Ivanov <semeni07@mail.ru>
2023-08-20 18:47:09 +0300

    Finished

--- todolistproject/tasks/models.py
+++ todolistproject/tasks/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class Task(models.Model):
     name = models.CharField(max_length=50, blank=False)
-    description = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=250, blank=False)
     is_done = models.BooleanField(default=False, blank=False)
     creation_date = models.DateField(default=datetime.now, blank=True)
 
